a64fd4534e31cf91b0ccfc6bd0040e50ebcc343b
salma hamdi <saalmahm51@gmail.com>
2025-03-20 14:06:14 +0000

    Réaliser l'authentification via Laravel Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            "name" => "required|string|max:300",
+            "email" => "required|string|max:300|email|unique:users",
+            "password" => "required|min:4"
+        ]);
+
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'register successful'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            "email" => "required|string|max:300|email",
+            "password" => "required|min:4"
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'bay bay']);
+    }
+}
