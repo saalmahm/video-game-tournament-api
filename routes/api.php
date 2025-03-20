bc7d2dfc07152e07a69b46450a62992048e7cf9e
salma hamdi <saalmahm51@gmail.com>
2025-03-20 14:13:56 +0000

    Récupération des informations de l'utilisateur authentifié

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,9 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
