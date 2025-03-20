96f32d3e451240cfe62c48c64d04220425eafaa7
salma hamdi <saalmahm51@gmail.com>
2025-03-20 19:40:33 +0000

    Appliquer l'inscription des Joueurs a un tournoi

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\PlayerController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -20,4 +21,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/tournaments/{id}', [TournamentController::class, 'show']); 
     Route::put('/tournaments/{id}', [TournamentController::class, 'update']);
     Route::delete('/tournaments/{id}', [TournamentController::class, 'destroy']); 
+
+    Route::post('/tournaments/{tournament_id}/players', [PlayerController::class, 'store']); 
+    Route::get('/tournaments/{tournament_id}/players', [PlayerController::class, 'index']); 
+    Route::delete('/tournaments/{tournament_id}/players/{player_id}', [PlayerController::class, 'destroy']); 
 });
